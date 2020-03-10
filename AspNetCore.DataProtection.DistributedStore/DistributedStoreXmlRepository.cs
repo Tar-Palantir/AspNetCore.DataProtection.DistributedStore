29c60cea6202a5a1bba6a2a6b7ed7d7c18a372f1
astaldo <astaldo@yeah.net>
2020-03-10 11:52:05 +0800

    commit code to github

---
+++ AspNetCore.DataProtection.DistributedStore/DistributedStoreXmlRepository.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+using Microsoft.AspNetCore.DataProtection.Repositories;
+using Microsoft.Extensions.Caching.Distributed;
+
+namespace Fisho.Framework.Service.DataProtection.DistributedStoreRedis
+{
+    public class DistributedStoreXmlRepository: IXmlRepository
+    {
+        private readonly IDistributedCache _cache;
+        private readonly string _key;
+
+        public DistributedStoreXmlRepository(
+            IDistributedCache cache,
+            string key)
+        {
+            _cache = cache ?? throw new ArgumentNullException(nameof(cache));
+
+            _key = key;
+        }
+
+        public IReadOnlyCollection<XElement> GetAllElements()
+        {
+            var data = _cache.GetString(_key);
+            if(!string.IsNullOrEmpty(data))
+            {
+                return XDocument.Parse(data).Root.Elements().ToList().AsReadOnly();
+            }
+            else
+            {
+                return new List<XElement>().AsReadOnly();
+            }
+        }
+
+        public void StoreElement(XElement element, string friendlyName)
+        {
+            var data = _cache.GetString(_key);
+
+            XDocument doc = string.IsNullOrEmpty(data) ?
+                new XDocument(new XElement("keys")) : XDocument.Parse(data);
+            doc.Root.Add(element);
+
+            _cache.SetString(_key, doc.ToString());
+        }
+    }
+}
---
+++ AspNetCore.DataProtection.DistributedStore/PersistKeysToDistributedStoreExtensions.cs
@@ -0,0 +1,31 @@
+using System;
+using Microsoft.AspNetCore.DataProtection;
+using Microsoft.AspNetCore.DataProtection.KeyManagement;
+using Microsoft.AspNetCore.DataProtection.Repositories;
+using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+
+namespace Fisho.Framework.Service.DataProtection.DistributedStoreRedis
+{
+    public static class PersistKeysToDistributedStoreExtensions
+    {
+        public static IDataProtectionBuilder PersistKeysToDistributedStore(
+            this IDataProtectionBuilder builder, string key = "DataProtection-Keys")
+        {
+            if (builder == null)
+                throw new ArgumentNullException(nameof(builder));
+            
+            builder.Services.AddSingleton(
+                services =>
+                {
+                    var cache = services.GetService<IDistributedCache>();
+                    return (IConfigureOptions<KeyManagementOptions>) new ConfigureOptions<KeyManagementOptions>(
+                        options =>
+                            options.XmlRepository =
+                                (IXmlRepository) new DistributedStoreXmlRepository(cache, key));
+                });
+            return builder;
+        }
+    }
+}
